9552fb449f0438c66c1fce3d6dcf78444ee2d0f1
Andrew Janong <andrewjanong@gmail.com>
2023-12-31 08:22:50 +0700

    remove env import

--- database/database.go
+++ database/database.go
@@ -5,17 +5,17 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 // Loads Database URI from .env file
 func loadDbUri() string {
-	err := godotenv.Load("database/.env")
+	// err := godotenv.Load("database/.env")
 
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	log.Printf("Env value: %s", os.Getenv("DATABASE_URI"))
 
